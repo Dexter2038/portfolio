c506e53c37c79731721a6c133e55eb595e5a5e08
Velmer <barkaloff.m@gmail.com>
2025-06-09 18:22:48 +0300

    new layout

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,17 +1,21 @@
-import { Header } from "@/components/header"
-import { Greeting } from "@/components/greeting"
-import { Nav } from "@/components/nav"
+import { Profile } from "@/components/profile"
+import { Process } from "@/components/process"
+import { Experience } from "@/components/experience"
 import { Stack } from "@/components/stack"
 import { Projects } from "@/components/projects"
 import { Contacts } from "@/components/contacts"
+import { CTA } from "@/components/cta"
 
 export function Main() {
-  return (<>
-    <Header />
-    <Greeting />
-    <Nav />
-    <Stack />
-    <Projects />
-    <Contacts />
-  </>)
+  return (
+    <div>
+      <Profile />
+      <Stack />
+      <Process />
+      <Experience />
+      <Projects />
+      <Contacts />
+      <CTA />
+    </div>
+  )
 }
--- src/components/stack.tsx
+++ src/components/stack.tsx
@@ -1,3 +1,26 @@
+import { Wrench } from "lucide-react";
+
 export function Stack() {
+  return (
+    <section id="stack" className="p-10 border-t-current border-t-4">
+      <span className="flex items-center pb-2">
+        <Wrench />
+        <h1 className="text-3xl px-2">Мой стек</h1>
+      </span>
+      <ol>
+        <li>
+          <p>
+            Rust
+          </p>
+          <p>
+            Python
+          </p>
+          <p>
+            JavaScript
+          </p>
+        </li>
+      </ol>
+    </section>
+  )
 
 }
