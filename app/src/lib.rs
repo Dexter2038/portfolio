0b83c5b50713c1bdf21c0f5528e9c1c44444d4d0
Dexter2038 <116676321+Dexter2038@users.noreply.github.com>
2025-07-28 17:43:45 +0300

    stack is ready now and layout is remade again

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,4 +1,3 @@
-use leptos::logging::*;
 use leptos::prelude::*;
 use leptos_meta::{provide_meta_context, Title};
 use leptos_router::{
@@ -64,28 +63,95 @@ fn Personal() -> impl IntoView {
     }
 }
 
+struct Stack {
+    pub name: &'static str,
+    pub icon: &'static str,
+    pub experience: i32,
+}
+
+impl Stack {
+    fn new(name: &'static str, icon: &'static str, experience: i32) -> Self {
+        Self {
+            name,
+            icon,
+            experience,
+        }
+    }
+}
+
 #[component]
 fn Stack() -> impl IntoView {
+    let stack = vec![
+        Stack::new("Python", "https://simpleicons.org/icons/python.svg", 24),
+        Stack::new(
+            "JavaScript",
+            "https://simpleicons.org/icons/javascript.svg",
+            12,
+        ),
+        Stack::new(
+            "TypeScript",
+            "https://simpleicons.org/icons/typescript.svg",
+            10,
+        ),
+        Stack::new("Rust", "https://simpleicons.org/icons/rust.svg", 5),
+        Stack::new("Golang", "https://simpleicons.org/icons/go.svg", 2),
+        Stack::new(
+            "PostgreSQL",
+            "https://simpleicons.org/icons/postgresql.svg",
+            18,
+        ),
+        Stack::new("Docker", "https://simpleicons.org/icons/docker.svg", 10),
+        Stack::new("Linux", "https://simpleicons.org/icons/linux.svg", 7),
+        Stack::new("Git", "https://simpleicons.org/icons/git.svg", 24),
+        Stack::new("Redis", "https://simpleicons.org/icons/redis.svg", 15),
+        Stack::new("FastAPI", "https://simpleicons.org/icons/fastapi.svg", 20),
+        Stack::new("Aiogram", "/aiogram.svg", 18),
+        Stack::new("Leptos", "https://simpleicons.org/icons/leptos.svg", 1),
+        Stack::new("Axum", "https://simpleicons.org/icons/tokio.svg", 4),
+        Stack::new("React", "https://simpleicons.org/icons/react.svg", 10),
+        Stack::new("Next.js", "https://simpleicons.org/icons/nextdotjs.svg", 8),
+        Stack::new("Gin", "https://simpleicons.org/icons/gin.svg", 1),
+    ];
     view! {
         <div class="flex flex-col basis-1/3 border-black bg-gray-100 shadow rounded-3xl p-x-4 flex-1">
             <h1 class="text-xl">Stack</h1>
             <div class="flex flex-row justify-stretch">
-                <ul class="w-full basis-2/3">
-                    <li>Leptos</li>
-                    <li>Tailwind</li>
-                    <li>Axum</li>
-                    <li>FastAPI</li>
-                    <li>React</li>
-                    <li>Next.js</li>
-                </ul>
-                <div class="w-full basis-1/3">
+                <div class="w-full flex flex-row gap-2 basis-3/5 p-4">
                     <ul>
-                        <li>Leptos</li>
-                        <li>Tailwind</li>
-                        <li>Axum</li>
-                        <li>FastAPI</li>
-                        <li>React</li>
-                        <li>Next.js</li>
+                        {stack.iter().map(|s| view!{
+                            <li class="flex flex-row gap-2 items-center">
+                                <img src="/point.svg" class="w-[20px] h-[20px]" />
+                                <p>{s.name}</p>
+                            </li>
+                        }).collect::<Vec<_>>()}
+                    </ul>
+                    <ul class="flex flex-col">
+                        {stack.iter().map(|s| view!{
+                            <li class="flex flex-row gap-2 items-center">
+                                <img src={s.icon} class="w-[20px] h-[20px]" />
+                                <p>{s.experience} year{if s.experience == 1 {""} else {"s"}}</p>
+                            </li>
+                        }).collect::<Vec<_>>()}
+                    </ul>
+                </div>
+                <div class="w-full basis-2/5 bg-gray-200 shadow rounded-2xl p-2 m-2 box-content">
+                    <ul class="flex flex-col gap-5 p-2">
+                        {
+                            let max_experience = stack.iter().map(|s| s.experience).max().unwrap_or(0);
+                            stack.iter().map(|s| {
+                                let percentage = (s.experience as f32 / max_experience as f32) * 100.0;
+                                let color = match percentage {
+                                    0.0..=33.9 => "bg-red-500",
+                                    34.0..=66.0 => "bg-yellow-500",
+                                    _ => "bg-green-500",
+                                };
+                                view! {
+                                    <li>
+                                        <div class=format!("h-[4.3px] w-[{percentage}%] {color}")></div>
+                                    </li>
+                                }
+                            }).collect::<Vec<_>>()
+                        }
                     </ul>
                 </div>
             </div>
@@ -161,13 +227,17 @@ fn Contacts() -> impl IntoView {
 fn MainPage() -> impl IntoView {
     view! {
         // Tailwind
-        <div class="flex flex-col flex-wrap gap-4 w-full h-full">
-            <Personal />
+        <div class="flex flex-col gap-4 w-full h-full">
+            <div class="flex flex-row gap-4 w-full h-full">
+                <Personal />
+                <Stack />
+                <Process />
+            </div>
+            <div class="flex flex-row gap-4 w-full h-full">
                 <Experience />
-            <Stack />
                 <Projects />
-            <Process />
                 <Contacts />
+            </div>
         </div>
     }
 }
