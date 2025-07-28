64c4ab8db38a8df85804f1fc7b6ebec22601dfbf
Dexter2038 <116676321+Dexter2038@users.noreply.github.com>
2025-07-28 22:33:45 +0300

    work process ready

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use leptos::prelude::*;
 use leptos_meta::{provide_meta_context, Title};
 use leptos_router::{
@@ -165,17 +167,54 @@ fn Stack() -> impl IntoView {
     }
 }
 
+struct Step {
+    title: &'static str,
+    description: &'static str,
+}
+
+impl Step {
+    fn new(title: &'static str, description: &'static str) -> Self {
+        Self { title, description }
+    }
+}
+
 #[component]
 fn Process() -> impl IntoView {
+    let steps = vec![
+        Step::new(
+            "Project Discussion",
+            "We chat to clarify your goals, features, timeline and budget. You can share references, sketches, or anything helpful."
+        ),
+        Step::new(
+            "Proposal & Agreement",
+            "I send a clear project outline with scope, timeline, and price. Once confirmed, we move forward."
+        ),
+        Step::new(
+            "Upfront Payment & Start",
+            "A 30–50% upfront payment secures the project. I begin work based on the agreed plan."
+        ),
+        Step::new(
+            "Progress Updates & Feedback",
+            "You receive updates at key milestones. I welcome feedback and make any needed adjustments."
+        ),
+        Step::new(
+            "Final Delivery & Payment",
+            "After final approval, you make the remaining payment. I deliver everything as promised — ready to launch or hand over."
+        )
+    ];
     view! {
-        <div class="flex flex-col basis-1/3 border-black  bg-gray-100 shadow rounded-3xl p-4 flex-1">
-            <div class="flex flex-row justify-between">
-                <img src="/portfolio-photo.jpg" class="w-[200px] h-[200px] rounded-3xl" />
-                <div class="flex flex-col justify-center w-full">
-                </div>
-            </div>
-            <div class="flex flex-col justify-center align-items h-full">
-                Process
+        <div class="flex flex-col basis-1/3 border-black bg-gray-100 shadow rounded-3xl p-4 flex-1">
+            <h1 class="text-xl">How does work process go</h1>
+            <div class="flex flex-col justify-stretch">
+                {steps.iter().enumerate().map(|(i, s)| view!{
+                    <div class="flex flex-col">
+                        <div class="flex flex-row gap-1">
+                            <p>{i + 1}.</p>
+                            <p class="font-bold">{s.title}</p>
+                        </div>
+                        <p class="text-right text-gray-600">{s.description}</p>
+                    </div>
+                }).collect::<Vec<_>>()}
             </div>
         </div>
     }
