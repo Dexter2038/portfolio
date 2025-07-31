1865b532a2bb8c7140c7f7c48021c9d6be159f4d
Dexter2038 <116676321+Dexter2038@users.noreply.github.com>
2025-07-31 12:38:16 +0300

    added experience section

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,6 +1,4 @@
-use std::collections::HashMap;
-
-use leptos::prelude::*;
+use leptos::{attr::Datetime, prelude::*};
 use leptos_meta::{provide_meta_context, Title};
 use leptos_router::{
     components::{Route, Router, Routes},
@@ -220,17 +218,85 @@ fn Process() -> impl IntoView {
     }
 }
 
+struct Experience {
+    pub orderer: &'static str,
+    pub description: &'static str,
+    pub role: &'static str,
+    pub start: &'static str,
+    pub end: &'static str,
+}
+
+impl Experience {
+    fn new(
+        orderer: &'static str,
+        description: &'static str,
+        role: &'static str,
+        start: &'static str,
+        end: &'static str,
+    ) -> Self {
+        Self {
+            orderer,
+            description,
+            role,
+            start,
+            end,
+        }
+    }
+}
+
 #[component]
-fn Experience() -> impl IntoView {
+fn Experiences() -> impl IntoView {
+    let experiences = vec![
+        Experience::new(
+            "Dead Inside",
+            r#"
+                Delivered product successuffuly after payment,
+                then fixes, new updates and still available on
+                Telegram
+            "#,
+            "Bot developer",
+            "01.01.2023",
+            "01.01.2024",
+        ),
+        Experience::new(
+            "IT Programmer",
+            r#"
+                Some random text, because I can't really remember,
+                it was quite long ago and I don't want to remember it
+                anymore
+            "#,
+            "Fullstack developer",
+            "01.06.2024",
+            "01.09.2024",
+        ),
+        Experience::new(
+            "IT Programmer",
+            r#"
+                Some random text, because I can't really remember,
+                it was quite long ago and I don't want to remember it
+                anymore
+            "#,
+            "Fullstack developer",
+            "01.06.2024",
+            "01.09.2024",
+        ),
+    ];
     view! {
         <div class="flex flex-col basis-1/3 border-black bg-gray-100 shadow rounded-3xl p-4 flex-1">
-            <div class="flex flex-row justify-between">
-                <img src="/portfolio-photo.jpg" class="w-[200px] h-[200px] rounded-3xl" />
-                <div class="flex flex-col justify-center w-full">
-                </div>
-            </div>
-            <div class="flex flex-col justify-center align-items h-full">
-                Experience
+            <h1 class="text-xl">Experience</h1>
+            <div class="flex flex-col justify-center align-items h-full w-full gap-4">
+                {experiences.iter().map(|e| view!{
+                    <div class="flex flex-row gap-1 rounded-3xl border-black border p-4">
+                        <div class="flex flex-col gap-1">
+                            <p>{e.orderer}</p>
+                            <p class="text-gray-600 text-sm">{e.start} - {e.end}</p>
+                        </div>
+                        <div class="flex flex-col justify-around">
+                            <p class="text-right">{e.description}</p>
+                            <p class="text-gray-600">{e.role}</p>
+                        </div>
+                    </div>
+                }).collect::<Vec<_>>()}
             </div>
         </div>
     }
@@ -351,7 +417,7 @@ fn MainPage() -> impl IntoView {
                 <Process />
             </div>
             <div class="flex flex-row gap-4 w-full h-full">
-                <Experience />
+                <Experiences />
                 <Projects />
                 <Contacts />
             </div>
